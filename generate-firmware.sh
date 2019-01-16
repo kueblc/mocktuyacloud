31df1811a5d9c876fc0dbb1c678ebe59ee434285
Colin Kuebler <kueblc@gmail.com>
2019-01-16 15:05:01 -0500

    Move second stage firmware into the correct folder for stage two

--- generate-firmware.sh
+++ generate-firmware.sh
@@ -8,5 +8,7 @@ cat header image_user2-0x81000.bin image_user2-0x81000.bin > firmware.bin
 
 # download stage 2
 wget https://github.com/arendst/Sonoff-Tasmota/releases/download/v6.2.1/sonoff.bin
+mkdir -p ota
+mv sonoff.bin ota/image_arduino.bin
 
 cd ..
