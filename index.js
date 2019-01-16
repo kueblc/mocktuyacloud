71feae1cc829df3a46223ee3e1602a435a81d938
Colin Kuebler <kueblc@gmail.com>
2019-01-16 15:18:20 -0500

    Ask devices to upgrade via MQTT if we can (note that this requires keys to work) Only ask devices to move if they're not on our mock network Customized two more upgrade endpoints to serve firmware, yet to be fully tested and debugged

--- index.js
+++ index.js
@@ -13,9 +13,19 @@ function reconnectDevices( ssid, passwd, ids = [] ){
 	TuyaDeviceMonitor.on( 'new', device => {
 		const { gwId: id, ip, productKey, version } = device
 		log.info( "found device", id, "at", ip )
-		if( ids.includes(id) || !ids.length ){
+		if( ids.length && !ids.includes(id) ) return
+		if( ip.startsWith("192.168.12.") ){
+			// these devices are connected to the new network
+			// ask them to upgrade in a second
+			log.info( "requesting", id, "to upgrade via MQTT" )
+			setTimeout( () => TuyaMQTTServer.updateFirmware(id), 1000 )
+		} else {
+			// these devices are connected to the old network
+			// move them to the new network
 			log.info( "moving", id, "to", ssid )
 			TuyaLocalControl.apConfig( ip, ssid, passwd )
+			// let it be found again
+			delete TuyaDeviceMonitor.devices[ id ]
 		}
 	})
 
@@ -25,15 +35,51 @@ function reconnectDevices( ssid, passwd, ids = [] ){
 // override the tuya.device.upgrade.silent.get api call
 const API = require('./lib/api')
 
+const upgrade = {
+	"url": "http://a.tuyaus.com/firmware.bin",
+	"size": "478491",
+	"md5": "79e9748319d7ad888f2deff16a87a296",
+	"version": "4.2.0",
+}
+
+// many devices seem to call this on start up or on switching to a new network
 API["tuya.device.upgrade.silent.get"] = params => {
 	if( ids.includes(params.gwId) || !ids.length )
 		return {
-			"url": "http://a.tuyaus.com/firmware.bin",
+			"url": upgrade.url,
 			"type": 0,
-			"size": "478491",
-			"md5": "79e9748319d7ad888f2deff16a87a296",
-			"version": "4.2.0",
+			"size": upgrade.size,
+			"md5": upgrade.md5,
+			"version": upgrade.version
 		}
+}
+
+// older devices call this to upgrade firmware when protocol 15 comes over mqtt
+// FIXME format looks good yet devices won't take, something must be missing
+// or perhaps some devices just don't accept OTA upgrades
+API["s.gw.upgrade"] = params => {
+	const etag = params.data && params.data.etag || "0000000xxx"
+	if( !etag ) return
+	return {
+		"fileSize": upgrade.size,
+		"etag": etag,
+		"version": upgrade.version,
+		"url": upgrade.url,
+		"md5": upgrade.md5
+	}
+}
+
+// newer devices call this to upgrade firmware when protocol 15 comes over mqtt
+API["tuya.device.upgrade.get"] = params => {
+	return {
+		"size": upgrade.size,
+		"cdnUrl": upgrade.url,
+		"originalUrl": upgrade.url,
+		"httpsUrl": upgrade.url,
+		"version": upgrade.version,
+		"url": upgrade.url,
+		"md5": upgrade.md5
+	}
 }
 
 // link any new devices, MockTuyaApi handles issuing keys
