890cc47c3ca7089212732fc907466f37db44a532
Colin Kuebler <kueblc@gmail.com>
2019-01-17 20:27:15 -0500

    Fixed API response for s.gw.upgrade and successfully flash a device via MQTT command Included modified version of tuyapi/link that tends to work a bit better for me, not sure if it'll help anyone else TODO: rewrite link.js with new data from captures Put linking on a 5 second loop, so you can put devices into config mode any time Linking is a bit fickle as it is, so keeping it on a loop gives devices plenty of opportunities to connect

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ function reconnectDevices( ssid, passwd, ids = [] ){
 		if( ids.length && !ids.includes(id) ) return
 		if( ip.startsWith("192.168.12.") ){
 			// these devices are connected to the new network
-			// ask them to upgrade in a second
-			log.info( "requesting", id, "to upgrade via MQTT" )
-			setTimeout( () => TuyaMQTTServer.updateFirmware(id), 1000 )
+			// ask them to upgrade in a couple seconds
+			// just in case they don't automatically request upgrades
+			log.info( "requesting", id, "to upgrade via MQTT in 5s" )
+			setTimeout( () => TuyaMQTTServer.updateFirmware(id), 5000 )
 		} else {
 			// these devices are connected to the old network
 			// move them to the new network
@@ -54,22 +55,19 @@ API["tuya.device.upgrade.silent.get"] = params => {
 		}
 }
 
-// older devices call this to upgrade firmware when protocol 15 comes over mqtt
-// FIXME format looks good yet devices won't take, something must be missing
-// or perhaps some devices just don't accept OTA upgrades
+// some devices call this to upgrade firmware when protocol 15 comes over mqtt
 API["s.gw.upgrade"] = params => {
-	const etag = params.data && params.data.etag || "0000000xxx"
-	if( !etag ) return
 	return {
-		"fileSize": upgrade.size,
-		"etag": etag,
-		"version": upgrade.version,
+		"auto": true,
 		"url": upgrade.url,
-		"md5": upgrade.md5
+		"size": upgrade.size,
+		"fileSize": upgrade.size,
+		"md5": upgrade.md5,
+		"version": upgrade.version
 	}
 }
 
-// newer devices call this to upgrade firmware when protocol 15 comes over mqtt
+// other devices call this to upgrade firmware when protocol 15 comes over mqtt
 API["tuya.device.upgrade.get"] = params => {
 	return {
 		"size": upgrade.size,
@@ -83,19 +81,28 @@ API["tuya.device.upgrade.get"] = params => {
 }
 
 // link any new devices, MockTuyaApi handles issuing keys
+// this is based on @tuyapi/link.manual
 // I tweaked some timing in my version that speeds it up and possibly more reliable
 // I oughta do some more testing and submit changes upstream
-const TuyaLink = require('@tuyapi/link').manual
+const TuyaLink = require('./lib/link')
 const link = new TuyaLink
 
-log.info("prompting unlinked devices to connect to the network")
-link.registerSmartLink({
-	region: 'AZ',
-	token: 'abcdefgh',
-	secret: '0000',
-	ssid: config.ssid,
-	wifiPassword: config.passwd,
-}).then(() => link.cleanup())
+function smartLink(){
+	log.info("prompting unlinked devices to connect to the network")
+	link.registerSmartLink({
+		region: 'AZ',
+		token: 'abcdefgh',
+		secret: '0000',
+		ssid: config.ssid,
+		wifiPassword: config.passwd,
+	}).then(() => {
+		setTimeout( smartLink, 5000 ) // try again in 5 seconds
+		// link.cleanup()
+	})
+}
+
+// start polling for new unlinked devices
+setTimeout( smartLink, 5000 )
 
 // move existing devices to the new network
 reconnectDevices( config.ssid, config.passwd, ids )
